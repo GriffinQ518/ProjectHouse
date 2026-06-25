571deb5689eed45aeac424c1e9482fcbaed0b38d
Griffin <griffinquayle@gmail.com>
2026-06-24 20:33:54 -0400

    Redid box_moveable and box_maskable, preliminary set up for pedestal functionality.

---
+++ PrototypeScene/box_maskable.gd
@@ -0,0 +1,8 @@
+extends MeshInstance3D
+
+@export var offset: Vector3
+@export var target: Node3D
+
+func _process(delta: float) -> void:
+	self.global_position = target.global_position + offset
+	pass
